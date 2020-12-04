6086a4d23370743aa61ceeb40b45819fb6c32535
Dylon-Colquitt <dragon12907@gmail.com>
2020-12-04 11:42:20 -0500

    update

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,31 @@
+import java.util.HashMap;
+import java.util.Collection;
+import java.util.Map;
+
 public class Bank {
+    Map<String, Account> accounts = new HashMap<String, Account>();
+
+    public void addAccount(Account inNewAccount){
+        accounts.put(inNewAccount.getAccountNumber(), inNewAccount);
+        //Key is inNewAccount.getAccountNumber()
+        //Value inNewAccount
+    }
+
+    //below, we are asking to get the account number. As such in the parameter (), we ask it to get account number
+    //Which is in the form of a string shown in our **Account** class
+    public Account getAccount(String accountNumber){
+        return accounts.get(accountNumber);
+        //the return above allows it to look for the account number and return it.
+    }
+
+    public Collection<Account> getAllAccounts(){
+        return accounts.values();
+    }
+
+    public void closeAccount (String accountNumber){
+        accounts.remove(accountNumber);
+        //.remove allows us to remove something from the map.
+        //within the parameter () we place what we want the value to be that we want to be removed. Which in this case
+        //we will be asking it to remove a specific accountNumber.
+    }
 }
---
+++ src/main/java/BankingApp.java
@@ -0,0 +1,48 @@
+import java.util.Collection;
+import java.util.Scanner;
+
+public class BankingApp {
+    public static void main(String[] args) {
+        Bank chaseBank = new Bank();
+        Account account1 = new Account("777", "Checking",1000);
+        Account account2 = new Account("333","Savings",10000);
+
+        chaseBank.addAccount(account1);
+        chaseBank.addAccount(account2);
+        //Above we are adding the account numbers into the map. The .addAccount had been defined in the **Bank** class
+        //under line 8.
+        String action = "0";
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Which account do you wish to access?");
+            String choice = input.nextLine();
+
+            Account currentAccount = chaseBank.getAccount(choice);
+            //for the above, we are using the chaseBank.getAccount to call on the getAccount method on **Bank** class
+            //We then place in the parameter that the choice input the user types it will pull up the account
+
+            if (currentAccount!=null){
+                //If the user enters the right account number, meaning it doesn't equate to null, then it will print the
+                //info below
+                System.out.println("What do you want to do?");
+                System.out.println("1. to withdraw");
+                System.out.println("2. to deposit");
+                System.out.println("3. to get account balance");
+                System.out.println("4. to exit");
+
+                action = input.nextLine();
+                if(action.equals("1")){
+                    System.out.println("How much do you want to withdraw?");
+                    double amount = input.nextDouble();
+                }
+
+            } else{
+                //if user enters in the incorrect account number, it will return null and it will skip to the else
+                //section here.
+                System.out.println("incorrect account number!");
+            }
+        } while (!action.equals("4"));
+
+    }
+}
